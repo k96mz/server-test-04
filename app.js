9f95af9a0a415f28a85a34c527bdda7e2aba0c15
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-12-07 14:18:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express')
 const spdy = require('spdy') //for https
 const cors = require('cors') 
 const morgan = require('morgan')
-const MBTiles = require('@mapbox/mbtiles')
 //const TimeFormat = require('hh-mm-ss')
 const winston = require('winston')
 const DailyRotateFile = require('winston-daily-rotate-file')
